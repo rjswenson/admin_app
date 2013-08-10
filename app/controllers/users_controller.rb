cc1ffbd62d64d5ae3a2aba7248ec79cee66f8f43
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-10 15:03:05 -0700

    index pulls @users from database and displays

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
   def new  #show the new user form GET
+    @user = User.new
   end
 
   def create  #accept new data and store w/in database POST
+    @user = User.new
+    if @user.save(user_params)
+      redirect_to @user
+    else                      #redirect to new if save didn't work
+      render 'new'
+    end
   end
 
   def index   #show all users GET
+    @users = User.all
   end
 
   def show   #show particular user GET
@@ -18,6 +26,12 @@ class UsersController < ApplicationController
   end
 
   def update #send data to database PATCH
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
   end
 
 end
