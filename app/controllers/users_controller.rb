6910168de3c3b256d6ba3e0b0ec9395723a80b08
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-10 16:17:15 -0700

    fixed new adding to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,27 @@
-class UsersController < ApplicationController
-  def new  #show the new user form GET
+  class UsersController < ApplicationController
+  def new #show the new user form GET
     @user = User.new
   end
 
-  def create  #accept new data and store w/in database POST
-    @user = User.new
-    if @user.save(user_params)
-      redirect_to @user
-    else                      #redirect to new if save didn't work
+  def create #accept new data and store w/in database POST
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to :users
+    else #redirect to new if save didn't work
       render 'new'
     end
   end
 
-  def index   #show all users GET
+  def index #show all users GET
     @users = User.all
   end
 
-  def show   #show particular user GET
+  def show #show particular user GET
   end
 
   def destroy #deletes a user DELETE
+    User.find(params[:id]).destroy
+    redirect_to '/users'
   end
 
   def edit #show edit update form GET
@@ -31,7 +33,8 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+      params.require(:user).permit(:first_name, :email, :last_name)
   end
 
 end
+
