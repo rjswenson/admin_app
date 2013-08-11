aca92ebf0ae46a698925f61456af1e19c201620f
Robin Swenson-Healey <rjswenson@gmail.com>
2013-08-10 20:28:51 -0700

    modified css to center buttons and layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true, length: { maximum: 30 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
 end
